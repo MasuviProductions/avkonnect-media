2bd6ed2fa4b7cf1a65bd60bd2b860b468c3e8a28
guthal <37868861+guthal@users.noreply.github.com>
2023-05-28 08:39:21 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { fastify } from 'fastify';
 import initializeRoutes from './routes';
 import { initDynamoDB } from './utils/db/client';
+import fastifyCors from '@fastify/cors';
 
 const APP = fastify({
     logger: true,
 });
+APP.register(fastifyCors);
 
 initDynamoDB();
-
 initializeRoutes(APP);
 
 export { APP };
